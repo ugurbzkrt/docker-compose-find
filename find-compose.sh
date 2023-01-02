6aee83eac857688831ff6d009c7dfc74efaf40b6
Ugur Bozkurt <94750332+ugurbzkrt@users.noreply.github.com>
2023-01-02 13:08:10 +0300

    Update find-compose.sh

--- find-compose.sh
+++ find-compose.sh
@@ -1,6 +1,6 @@
 /usr/local/bin/dkcompose.sh
 
-
+```
 #!/bin/bash
 # register docker-compose commands and where they were launched.
 #------------
@@ -10,5 +10,18 @@ DIR=$(pwd)
 echo "$NOW  Directory: $DIR" >> $LOG
 echo "$NOW  Command: docker-compose $*" >> $LOG
 docker-compose $*
-
+```
 #-- end --#
+
+
+
+```
+Don’t forget to "chmod a+rx /usr/local/bin/dkcompose.sh" to allow execution of the script. As the script is in a directory in the $PATH variable can be called from anywhere, so you could say
+```
+
+
+```
+cd /projects/myproject2
+/home/ajnouri/dkcompose.sh up
+```
+
